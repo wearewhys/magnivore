be723f7f575376d0ce25b0590bd46dcc6f34ace8
Jacopo Cascioli <jacopocascioli@gmail.com>
2017-11-20 17:33:34 +0100

    chore(setup): version bump to 0.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author='Jacopo Cascioli',
     author_email='jacopocascioli@gmail.com',
     license='Apache 2',
-    version='0.3.1',
+    version='0.4.0',
     packages=find_packages(),
     tests_require=[
         'pytest',
